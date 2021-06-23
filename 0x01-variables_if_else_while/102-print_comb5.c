f80d96d0e53e26529a5714be004bb39adbe2b06c
arioledavid <davidariole@gmail.com>
2021-06-23 15:45:01 -0700

    upload

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j-0; j < 100; j++)
 		{
 			if (i < j)
 			{
@@ -19,7 +19,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				if (i != 98 || j != 99);
 				{
 					putchar(',');
 					putchar(' ');
