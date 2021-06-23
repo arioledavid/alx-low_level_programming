8167391f6db0e8ecf21d5af5416e56383019df52
arioledavid <davidariole@gmail.com>
2021-06-23 15:30:19 -0700

    upload

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,14 +12,14 @@ int main(void)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for(l = 50; l < 58; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 55 || m !=56)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
