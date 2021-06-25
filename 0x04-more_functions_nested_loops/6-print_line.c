3ae52d0efe0a425dabf2bfdffd524b048fc77971
arioledavid <davidariole@gmail.com>
2021-06-24 17:41:19 -0700

    upload

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,14 +3,25 @@
 /**
  * print_line - print the character "_" n times to draw a line
  * @n: number of characters to draw
+ *
+ * Return: empty
  */
 
 void print_line(int n)
 {
-	int times = n;
+	int x;
 
-	for (times = n; times > 0; times--)
-		_putchar('_');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
