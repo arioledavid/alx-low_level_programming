135afba2f7ecdcdbec9bae5eef04a1f2398c124e
arioledavid <davidariole@gmail.com>
2021-06-24 17:22:12 -0700

    upload

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - Prints a triandle of squares accotding parameter
+ * @size: The size of the squares triangle
+ *
+ * Return: empty
+ */
+
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (sizze <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+
+			for (z = 0; z <=x; z++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+}
