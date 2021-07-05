3e1f5983179531c6c90a665cb52003ff03aa4b04
arioledavid <davidariole@gmail.com>
2021-07-05 07:59:05 -0700

    upload

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,18 +12,21 @@ unsigned int _strspn(char *s, char *accept)
 
 	f = 0;
 
-	for (i = 0; s[i] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == accept[j])
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			f++;
-			flag = 1;
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
 		}
 		if (flag == 0)
 		{
-			return (f);
+			return (f)
 		}
 	}
 	return (0);
 }
-
