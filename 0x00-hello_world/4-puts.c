473226fb45ee255a7c37dc760bc9f410ec62c690
arioledavid <davidariole@gmail.com>
2021-07-27 07:09:03 -0700

    upload

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-/* main- entry point
- * return 0- if successful
+
+/**
+ * main- entry point
+ * Return:0- if successful
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
