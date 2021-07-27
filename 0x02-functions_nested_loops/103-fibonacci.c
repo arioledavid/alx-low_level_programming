d0f3bdc20f22d0a393f4f1862188723feb5d9199
arioledavid <davidariole@gmail.com>
2021-07-26 20:26:21 -0700

    upload

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even fibonacci sequence up to 4,000,000
+ * Return:0 success
+ */
+
+int main(void)
+{
+	int sumevens = 0;
+	int a;
+	int b;
+	int sum = 1;
+
+	a = 1;
+	b = 1;
+
+	while (b < 4000000)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		if ((sum <= 4000000) && (sum % 2 == 0))
+			sumevens =+sum;
+	}
+	printf("%d\n", sumevens);
+
+	return (0);
+}
