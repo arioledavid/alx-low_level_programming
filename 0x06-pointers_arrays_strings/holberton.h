79da9a6fd514b186fa265694d3541e96a4fdeb21
arioledavid <davidariole@gmail.com>
2021-08-11 14:32:10 -0700

    upload

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _strcat - concatenate two strings 
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src);
+
+/**
+ * _strcat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)
+ * Return: concatenated string
+ */
