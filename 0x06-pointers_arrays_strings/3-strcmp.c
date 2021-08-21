829f6db432f0a074180a0a0f1db63bbf1e83c744
arioledavid <davidariole@gmail.com>
2021-08-21 10:58:51 -0700

    upload

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
-		i++;
-
-	if (*(s2 + i))
-		return (*(s1 + i) - *(s2 + i));
-	else 
-		return (0);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			retutn (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - * s2);
 }
