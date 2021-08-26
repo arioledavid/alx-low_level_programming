a186a0fb4f9b1eee3ac6aaa4af00cba8587a2fa8
arioledavid <davidariole@gmail.com>
2021-08-25 17:13:21 -0700

    upload

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,16 +2,17 @@
 
 /** 
  * factorial - find the factorial of n
- * @n: number 
- * Return: factorial of n, -1 if n is negative 
+ * @n: number
+ * Return: factorial of n, -1 if n is negative
  */
 
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+
 	else if (n == 0 || n == 1)
 		return (n);
-	else 
-		return (n * factorial(n-1));
+	else
+		return (n * factorial(n - 1));
 }
