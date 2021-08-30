87e93ea769dd71f06880cf796932ca70113cff97
arioledavid <davidariole@gmail.com>
2021-08-29 18:42:00 -0700

    upload

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -36,7 +36,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	else iff (*s2 == '*')
+	else if (*s2 == '*')
 		return (substring_match(s1, (s2 + 1), (s2 + 1)));
 	else
 		return (0);
