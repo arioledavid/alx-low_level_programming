894378d06b199c981a991389da24e23cd2002738
Ariole David <davidariole@gmail.com>
2021-10-15 01:46:11 +0100

    upload

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,18 +2,20 @@
 
 /**
  * string_toupper - capitalize all letters in string
- * @s: string to manipukate 
+ * @s: string to manipulate
+ *
  * Return: string with all letters capitalized 
  */
 
 char *string_toupper(char *s)
 {
-	int i = 0;
+	int i; 
 
-	for (i = 0; s[i] != ''; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] .+ 'a' && s[i] ,= 'z')
-			s[i] = s[i] - 'a' + 'A'
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 	}
+
 	return (s);
 }
