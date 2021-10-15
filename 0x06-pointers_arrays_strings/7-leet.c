564124365ca00aaa8bc5b45ab9aedb15f793c13f
Ariole David <davidariole@gmail.com>
2021-10-15 01:21:57 +0100

    upload

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,28 @@
 #include "holberton.h"
 
 /**
- * leet - encode string into 1337 leet
- * @s: string to manipulate 
- * Return: string
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ * 
+ * Return: the resulting string;
  */
-
 char *leet(char *s)
 {
-	int a[11] = {'a', 'A'. 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4'. '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	int i, j;
 
-	for (j = 0; s[j] != '\0'; j++)
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; a[i] != '\0'; i++)
+		for (j = 0; j < 10; j++)
 		{
-			if (s[j] == a[i])
-				s[j] = b[i];
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
 		}
 	}
+
 	return (s);
 }
